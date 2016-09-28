594bcabe1a0a8c58a445b8fb7c569cf01fac2ccb
ludens(한병호) <chungdan@coupang.com>
2016-09-28 10:37:41 +0900

    Initial Commit

---
+++ bibleMemorizer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  bibleMemorizer
+//
+//  Created by Coupang on 2016. 9. 28..
+//  Copyright © 2016년 Byoungho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bibleMemorizer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bibleMemorizer
+//
+//  Created by Coupang on 2016. 9. 28..
+//  Copyright © 2016년 Byoungho. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
